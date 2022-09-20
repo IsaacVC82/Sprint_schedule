11e865910179f214db8b1409a22f5821127d400c
Isaac8-alt <79585800+Isaac8-alt@users.noreply.github.com>
2022-09-20 15:40:34 -0500

    fisrt commit

---
+++ src/componentes/cards.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+
+function Tasks(){
+    return(
+        <><div className='card-container'>
+            <h1>Sprint 1</h1>
+            <div className="text-container">
+                <h3>historia de usuario</h3>
+                <p className="task">Crear el esqueleto del catalogo web</p>
+            </div>
+
+
+        </div><div className='card-container'>
+                <h1>Sprint 1</h1>
+                <div className="text-container">
+                    <h3>historia de usuario</h3>
+                    <p className="task">Diseñar la página principal</p>
+                </div>
+
+
+            </div>
+            <div className='card-container'>
+            <h1>Sprint 1</h1>
+           <div className="text-container">
+            <h3>historia de usuario</h3>
+            <p className="task">Diseñar la vista de las paginas secundarias</p>
+           </div>
+
+
+           <div className='card-container'>
+            <h1>Sprint 2</h1>
+           <div className="text-container">
+            <h3>historia de usuario</h3>
+            <p className="task">Crear la página de log in</p>
+           </div>
+          
+
+           <div className='card-container'>
+            <h1>Sprint 2</h1>
+           <div className="text-container">
+            <h3>historia de usuario</h3>
+            <p className="task">Crear la página de la historia de la empresa</p>
+           </div>
+          
+
+           </div>
+
+
+           <div className='card-container'>
+            <h1>Sprint 2</h1>
+           <div className="text-container">
+            <h3>historia de usuario</h3>
+            <p className="task">Crear el sistema de pedidos y su base de datos</p>
+           </div>
+          
+
+           </div>
+
+
+           <div className='card-container'>
+            <h1>Sprint 3</h1>
+           <div className="text-container">
+            <h3>historia de usuario</h3>
+            <p className="task">Testear la seguridad del catalogo web</p>
+           </div>
+          
+
+           </div>
+
+
+           <div className='card-container'>
+            <h1>Sprint 3</h1>
+           <div className="text-container">
+            <h3>historia de usuario</h3>
+            <p className="task">Testear la accesibilidad del catalogo web</p>
+           </div>
+
+           <div className='card-container'>
+            <h1>Sprint 3</h1>
+           <div className="text-container">
+            <h3>historia de usuario</h3>
+            <p className="task">Testear la seguridad de la base de datos</p>
+           </div>
+          
+
+           </div>
+          
+
+           </div>
+
+           </div>
+          
+
+           </div></>
+
+    );
+}
+export default Tasks;
